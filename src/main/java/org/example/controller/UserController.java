93d18134cdf6de525039a8f4afa91c79aad63a32
czmadzia2201 <magda.k.strzelczyk@wp.pl>
2021-04-20 07:04:46 +0200

    Branch with icons

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
